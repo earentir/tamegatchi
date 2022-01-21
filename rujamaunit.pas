68fd18d4560c93e841d722537830090af5824dbf
earentir <earentir@gmail.com>
2022-01-21 21:08:16 +0200

    new TImage for screens

--- rujamaunit.pas
+++ rujamaunit.pas
@@ -25,6 +25,7 @@ type
     pictoHome8: TImage;
     pictoHome9: TImage;
     ShadowImage: TImage;
+    ScreensImage: TImage;
     SpriteImage: TImage;
     MasterTimer: TTimer;
     procedure FormCreate(Sender: TObject);
@@ -46,6 +47,8 @@ var
   canMoveForm: boolean;
   mouseX, mouseY: integer;
   menuItems: array of string = ('home', 'health', 'food', 'yard', 'settings', 'bath', 'play', 'book', 'shop', 'exit');
+  imgRootPath: string;
+
 
 implementation
 
@@ -59,16 +62,18 @@ procedure TtamegatchiForm.FormCreate(Sender: TObject);
 var
   i: integer;
 begin
-  bgImage.Picture.PNG.LoadFromFile(GetCurrentDir + PathDelim + 'media\img\bg-lcd-off.png');
+  imgRootPath := GetCurrentDir + PathDelim + 'media' + PathDelim + 'img' + PathDelim;
+
+  bgImage.Picture.PNG.LoadFromFile(imgRootPath + 'bg-lcd-off.png');
 
   settingList := TStringList.Create;
-  settingList.Values['eggFrame'] := IntToStr(0);
+  settingList.Values['Frame'] := IntToStr(0);
+  settingList.Values['Room'] := 'home';
 
   for i := 0 to PictoMenuPanel.ControlCount - 1 do
   begin
-    (PictoMenuPanel.Controls[i] as TImage).Picture.PNG.LoadFromFile(GetCurrentDir + PathDelim +
-      'media\img\pictograms\' + menuItems[StrToInt(Copy(PictoMenuPanel.Controls[i].GetNamePath, Length('pictoHome') + 1, 2)) -
-      1] + '.png');
+    (PictoMenuPanel.Controls[i] as TImage).Picture.PNG.LoadFromFile(imgRootPath + 'pictograms' + PathDelim +
+      menuItems[StrToInt(Copy(PictoMenuPanel.Controls[i].GetNamePath, Length('pictoHome') + 1, 2)) - 1] + '.png');
   end;
 end;
 
@@ -78,43 +83,74 @@ var
   maskpicture: TPicture;
 begin
   maskpicture := TPicture.Create;
-  maskpicture.PNG.LoadFromFile(GetCurrentDir + PathDelim + 'media\img\bg-lcd-off.png');
+  maskpicture.PNG.LoadFromFile(imgRootPath + 'bg-lcd-off.png');
   SetShape(maskpicture.Bitmap);
 end;
 
 function AnimateObject(objectName: string; index: integer): string;
 var
-  imagefilename: array of string = ('-default', '-skewl1', '-skewr1', '-jump1', '-jump2', '-jump1', '-default', '-skewl1', '-skewr1');
+  imagefilename: array of string = ('default', 'skewl1', 'skewr1', 'move1', 'move2', 'move1', 'default', 'skewl1', 'skewr1');
 begin
-  if StrToInt(settingList.Values['eggFrame']) < Length(imagefilename) then
+  if StrToInt(settingList.Values['Frame']) < Length(imagefilename) then
   begin
-    Result := GetCurrentDir + PathDelim + 'media\img\' + objectName + PathDelim + objectName + imagefilename[index];
+    Result := imgRootPath + objectName + PathDelim + imagefilename[index];
   end
   else
   begin
-    settingList.Values['eggFrame'] := IntToStr(0);
-    Result := GetCurrentDir + PathDelim + 'media\img\' + objectName + PathDelim + objectName + imagefilename[0];
+    settingList.Values['Frame'] := IntToStr(0);
+    Result := imgRootPath + objectName + PathDelim + imagefilename[0];
   end;
 end;
 
 procedure TtamegatchiForm.MasterTimerTimer(Sender: TObject);
 begin
-  SpriteImage.Picture.png.LoadFromFile(AnimateObject('egg', StrToInt(settingList.Values['eggFrame'])) + '.png');
-  ShadowImage.Picture.png.LoadFromFile(AnimateObject('egg', StrToInt(settingList.Values['eggFrame'])) + '-shadow' + '.png');
-  settingList.Values['eggFrame'] := IntToStr(StrToInt(settingList.Values['eggFrame']) + 1);
+  SpriteImage.Picture.png.LoadFromFile(AnimateObject('cat\1\', StrToInt(settingList.Values['Frame'])) + '.png');
+  ShadowImage.Picture.png.LoadFromFile(AnimateObject('cat\1\', StrToInt(settingList.Values['Frame'])) + '-shadow' + '.png');
+  settingList.Values['Frame'] := IntToStr(StrToInt(settingList.Values['Frame']) + 1);
 end;
 
 procedure TtamegatchiForm.pictoHomeClick(Sender: TObject);
+var
+  roomFromMenu: string;
 begin
-  WriteLn('menu: ', menuItems[StrToInt(Copy((Sender as TImage).GetNamePath, Length('pictoHome') + 1, 2)) - 1]);
+  roomFromMenu := menuItems[StrToInt(Copy((Sender as TImage).GetNamePath, Length('pictoHome') + 1, 2)) - 1];
 
-  case menuItems[StrToInt(Copy((Sender as TImage).GetNamePath, Length('pictoHome') + 1, 2)) - 1] of
-    'home':
-    begin
+  //writeLn(roomFromMenu);
+  //writeLn(settingList.Values['Room']);
+  if (roomFromMenu = settingList.Values['Room']) then
+  begin
+    ScreensImage.Picture.PNG.Clear;
+    settingList.Values['Room'] := 'home';
+  end
+  else
+  begin
+    case menuItems[StrToInt(Copy((Sender as TImage).GetNamePath, Length('pictoHome') + 1, 2)) - 1] of
+      'home':
+        ScreensImage.Picture.PNG.LoadFromFile(imgRootPath + 'screens' + PathDelim + 'health.png');
+
+      'food':
+        ScreensImage.Picture.PNG.LoadFromFile(imgRootPath + 'screens' + PathDelim + 'health.png');
+
+      'yard':
+        ScreensImage.Picture.PNG.LoadFromFile(imgRootPath + 'screens' + PathDelim + 'health.png');
+
+      'health':
+        ScreensImage.Picture.PNG.LoadFromFile(imgRootPath + 'screens' + PathDelim + 'health.png');
+
+      'play':
+        ScreensImage.Picture.PNG.LoadFromFile(imgRootPath + 'screens' + PathDelim + 'health.png');
+
+      'path':
+        ScreensImage.Picture.PNG.LoadFromFile(imgRootPath + 'screens' + PathDelim + 'health.png');
+
+      'book':
+        ScreensImage.Picture.PNG.LoadFromFile(imgRootPath + 'screens' + PathDelim + 'health.png');
+
+      'exit':
+        Application.Terminate;
     end;
 
-    'exit':
-      Application.Terminate;
+    settingList.Values['Room'] := roomFromMenu;
   end;
 end;
 
